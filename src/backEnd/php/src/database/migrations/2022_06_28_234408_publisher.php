9e3ec25390b1f24ad406059a24539aa25a6cb049
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 13:12:28 -0400

    first version of publishers table

--- src/backEnd/php/src/database/migrations/2022_06_28_234408_publisher.php
+++ src/backEnd/php/src/database/migrations/2022_06_28_234408_publisher.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->dateTime('created_at');
+            $table->dateTime("updated_at")->nullable();
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('tags');
     }
 };
