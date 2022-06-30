6025b78211f250dcce789375591bb980420c0242
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 13:09:12 -0400

    first version of games table

---
+++ src/backEnd/php/src/database/migrations/2022_06_28_231806_games.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * Games should be stored in a persistent storage. The loose game schema is:
+        - publisher (string, required)
+        - name (string, required)
+        - nickname (string, can be null)
+        - rating (pick whatever you want, this can be numerical, or an enumeration like “favorite”,
+        “meh”, “dislike”… whatever you like, can be null)
+         */
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('publish_id');
+            $table->string('name');
+            $table->string("nickname")->nullable();
+            $table->integer("sum_rating")->nullable();
+            $table->dateTime('created_at');
+            $table->dateTime("updated_at");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
