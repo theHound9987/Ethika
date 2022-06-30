997fea6e03ad10b0b7aff41123eeac97d83e9cbc
Edward Aaron Russell <erussell9987@gmail.com>
2022-06-30 13:10:49 -0400

    first version of games_ratings table

--- src/backEnd/php/src/database/migrations/2022_06_28_234633_games_ratings.php
+++ src/backEnd/php/src/database/migrations/2022_06_28_234633_games_ratings.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('games_rating', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->unsignedBigInteger('game_id');
+            $table->integer("rating");
+            $table->dateTime('created_at');
+            $table->dateTime("updated_at")->nullable();
+
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notes');
     }
 };
